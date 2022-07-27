4f32fccfbd80cbd86345355fdc641e2fe279982d
drewrolla <andrewong007@gmail.com>
2022-07-27 13:34:23 -0400

    connected to PokeAPI

--- app/poke/poke_routes.py
+++ app/poke/poke_routes.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Blueprint, render_template, request, redirect, url_for
 from .forms import UserCreationForm
 
@@ -18,14 +19,22 @@ def getPokemon():
         print('Post request made.')
         if form.validate():
             poke_name = form.poke_name.data
-
-            print(poke_name)
-
-            pokemon = Poke(poke_name)
-
+            response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{poke_name}')
+            data = response.json()
+            pokeDict = {}
+            pokemon = data['name']
+            pokeDict[pokemon] = {
+                'name' : data['forms'][0]['name'],
+                'abilities' : data['abilities'][0]['ability']['name'],
+                'base_exp' : data['base_experience'],
+                'sprite' : data['sprites']['front_shiny'],
+                'attack' : data['stats'][0]['base_stat'],
+                'hp' : data['stats'][1]['base_stat'],
+                'defense' : data['stats'][2]['base_stat']
+                }
+            return pokeDict
             db.session.add(pokemon)
             db.session.commit()
-
             return redirect(url_for('app.getPokeInfo'))
         else:
             print('Validation failed')
