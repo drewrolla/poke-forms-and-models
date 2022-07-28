c679166a608bc801bec269218ef04ad33d3fb0d3
drewrolla <andrewong007@gmail.com>
2022-07-28 15:15:38 -0400

    added card

--- app/poke/forms.py
+++ app/poke/forms.py
@@ -2,6 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
-class UserCreationForm(FlaskForm):
+class PokemonSearchForm(FlaskForm):
     poke_name = StringField('Pokemon Name', validators=[DataRequired()])
     submit = SubmitField()
--- app/poke/poke_routes.py
+++ app/poke/poke_routes.py
@@ -1,12 +1,11 @@
 import requests
 from flask import Blueprint, render_template, request, redirect, url_for
-from .forms import UserCreationForm
+from .forms import PokemonSearchForm
 
 from app.models import Poke
 
 poke = Blueprint('poke', __name__, template_folder='poketemplates')
 
-from app.models import db
 
 @poke.route('/pokeinfo')
 def getPokeInfo():
@@ -14,32 +13,27 @@ def getPokeInfo():
 
 @poke.route('/pokemon', methods=["GET", "POST"])
 def getPokemon():
-    form = UserCreationForm()
+    form = PokemonSearchForm()
+    my_dict = {}
+
     if request.method == "POST":
         print('Post request made.')
         if form.validate():
             poke_name = form.poke_name.data
-            response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{poke_name}')
-            data = response.json()
-            pokeDict = {}
-            pokemon = data['name']
-            pokeDict[pokemon] = {
-                'name' : data['forms'][0]['name'],
-                'abilities' : data['abilities'][0]['ability']['name'],
-                'base_exp' : data['base_experience'],
-                'sprite' : data['sprites']['front_shiny'],
-                'attack' : data['stats'][0]['base_stat'],
-                'hp' : data['stats'][1]['base_stat'],
-                'defense' : data['stats'][2]['base_stat']
-                }
-            return pokeDict
-            db.session.add(pokemon)
-            db.session.commit()
-            return redirect(url_for('app.getPokeInfo'))
-        else:
-            print('Validation failed')
-    else:
-        print('GET request made')
 
-    return render_template('pokemon.html', form = form)
+            url = f"https://pokeapi.co/api/v2/pokemon/{poke_name}"
+            res = requests.get(url)
+            if res.ok:
+                data = res.json()
+                my_dict = {
+                    'name': data['name'],
+                    'ability': data['abilities'][0]['ability']['name'],
+                    'img_url': data['sprites']['front_shiny'],
+                    "hp": data['stats'][0]['base_stat'],
+                    'attack': data['stats'][1]['base_stat'],
+                    'defense': data['stats'][2]['base_stat']
+                }
+
+
+    return render_template('pokemon.html', form=form, pokemon=my_dict)
 
